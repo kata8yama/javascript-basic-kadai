0630b851cc087de479beeaf30ce28a6f5ac32468
kata8yama <162008868+kata8yama@users.noreply.github.com>
2024-06-30 22:40:53 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,14 @@
+// 朝のあいさつを出力する関数
+const sayGoodMorning = () => {
+  console.log('おはようございます');
+  console.log('一日頑張りましょう！');
+}
+
+sayGoodMorning();
+
+// 二乗の関数
+const double = (num) => {
+  return num * num;
+}
+
+console.log(double(10));
