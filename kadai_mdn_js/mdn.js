e32f7851beac03cb7f149967197735ad67f8ac57
kata8yama <162008868+kata8yama@users.noreply.github.com>
2024-07-02 22:03:53 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const memory = new Date();
+const year = memory.getFullYear();
+const month = memory.getMonth() + 1 ;
+const date = memory.getDate();
+
+console.log(year + '年' + month + '月' + date + '日' );
